b788008a99cb0d8b10b48e4f4e118e53557404f6
Juan <jrmediavilla@gmail.com>
2023-03-27 22:19:46 +0100

    Update Timeline.js

--- components/Timeline.js
+++ components/Timeline.js
@@ -5,6 +5,30 @@ function Timeline() {
     // GSAP ANIM?
     console.log("Timeline loading");
 
+    function handlePlayClick() {
+        const firstKeyframe = keyframeData[0]; // Get the first keyframe
+        const firstRectangle = firstKeyframe.rectangles[0]; // Get the first rectangle from the first keyframe
+        const elementId = firstRectangle.id; // Assign ID of the first rectangle to a variable
+        const width = firstRectangle.width; // Assign width of the first rectangle to a variable
+        const height = firstRectangle.height; // Assign height of the first rectangle to a variable
+
+        Animation(elementId, width, height);
+
+    }
+
+
+    function saveKeyframeData(keyframeIndex) {
+        setKeyframeData((prevKeyframeData) => {
+            const updatedKeyframeData = [...prevKeyframeData];
+            updatedKeyframeData[keyframeIndex - 1] = {
+                keyframe: keyframeIndex,
+                rectangles: [...rectangles],
+            };
+
+            return updatedKeyframeData;
+        });
+    }
+
 
     return (
         <>
@@ -15,6 +39,7 @@ function Timeline() {
                         href="#"
                         role="button"
                         className="primary"
+                        onClick={handlePlayClick}
                     >
                         Play
                     </a>
@@ -25,6 +50,7 @@ function Timeline() {
                         href="#"
                         role="button"
                         className="secondary"
+                        onClick={() => saveKeyframeData(1)}
                     >
                         Record Keyframe 1
                     </a>
@@ -34,7 +60,7 @@ function Timeline() {
                         href="#"
                         role="button"
                         className="secondary"
-
+                        onClick={() => saveKeyframeData(2)}
                     >
                         Record Keyframe 2
                     </a>
